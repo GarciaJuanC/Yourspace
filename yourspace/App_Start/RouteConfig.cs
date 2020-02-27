744e3fe01c4b65477d7e12074ed92b1e26a7a5fc
Juan Garcia <juandamgarcia@gmail.com>
2020-02-27 11:37:11 -0800

    connected to sign up

--- yourspace/App_Start/RouteConfig.cs
+++ yourspace/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace yourspace
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "SignUp", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
            
             );
         }
