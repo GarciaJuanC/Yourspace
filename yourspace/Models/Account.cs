455984a7745529031da7325262b16b4e48fec8d4
Juan Garcia <juandamgarcia@gmail.com>
2020-02-26 19:15:12 -0800

    just checking

--- yourspace/Models/Account.cs
+++ yourspace/Models/Account.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace yourspace.Models
 {
     public partial class Account
     {
         public int AccountId { get; set; }
+
+        [Display(Name ="Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Email required")]
         public string Email { get; set; }
         public string SaltValue { get; set; }
         public string HashedPass { get; set; }
