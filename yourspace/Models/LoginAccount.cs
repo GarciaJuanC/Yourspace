53df6774bf0db598c36f4e6e9358a92753c4bec3
Juan Garcia <juandamgarcia@gmail.com>
2020-03-04 09:06:23 -0800

    successful login

---
+++ yourspace/Models/LoginAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace yourspace.Models
+{
+    public class LoginAccount
+    {
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email required")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password required")]
+        public string Password { get; set; }
+
+
+    }
+}
