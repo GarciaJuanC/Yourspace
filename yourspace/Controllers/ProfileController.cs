e271e643007c501c8ec16d22bf4c0f398e5e7d85
beachcoder25 <cornish25@gmail.com>
2020-03-11 09:05:22 -0700

    Added Controller and View for Profile

---
+++ yourspace/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yourspace.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your profile page";
+            return View();
+        }
+    }
+}
