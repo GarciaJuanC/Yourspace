106957a8058bb507e77cde3e785e338002e9d777
beachcoder25 <cornish25@gmail.com>
2020-03-18 00:31:57 -0700

    Experimenting with .css

--- yourspace/App_Start/BundleConfig.cs
+++ yourspace/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace yourspace
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/Profile.css"));
         }
     }
 }
