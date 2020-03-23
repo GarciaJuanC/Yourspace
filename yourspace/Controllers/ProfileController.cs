a85c1aba727cc3a410a673a8fc249a44b56ac719
beachcoder25 <cornish25@gmail.com>
2020-03-22 23:26:49 -0700

    Can retrieve user-posts from db

--- yourspace/Controllers/ProfileController.cs
+++ yourspace/Controllers/ProfileController.cs
@@ -13,11 +13,14 @@ namespace yourspace.Controllers
         
         public ActionResult Index(UserAccount userAccount)
         {
+            IList<Posts> postList = new List<Posts>();
             // Query String for debugging
             //userAccount = db.UserAccount.Where(s => s.FirstName == "J").FirstOrDefault();
             ViewBag.FirstName = userAccount.FirstName;
             ViewBag.LastName = userAccount.LastName;
+            ViewBag.postList = db.Posts.Where(p => p.AccountId == userAccount.AccountId);
             
+
 
             return View();
         }
