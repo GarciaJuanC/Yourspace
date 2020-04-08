4e7d4aec8a364da1bd2ed53a65ccd9375bd3c8b0
Daniel Flores <daniel.flores03@student.csulb.edu>
2020-04-08 01:02:01 -0700

    friends list related stuff

---
+++ yourspace/Models/FriendList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yourspace.Models
+{
+    [Serializable]
+    public class FriendList
+    {
+        readonly List<UserAccount> friends = new List<UserAccount>();
+
+        public FriendList()
+        {
+
+        }
+
+        public FriendList(UserAccount acct)
+        {
+            friends.Add(acct);
+        }
+
+        public void AddFriend(UserAccount acct)
+        {
+            friends.Add(acct);
+        }
+    }
+}
