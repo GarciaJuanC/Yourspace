a2c2253330f8710190e157e121d722f88a62ddcc
beachcoder25 <cornish25@gmail.com>
2020-04-29 01:48:05 -0700

    UserHomePage added, very close to feed being completed

--- Database/FillPostsTable.sql
+++ Database/FillPostsTable.sql
@@ -1,15 +1,29 @@
 INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
-		(20, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vivamus fringilla feugiat velit.', CURRENT_TIMESTAMP);
+		(1, NULL, 'Juan Post 1', CURRENT_TIMESTAMP);
 
 INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
-		(20, NULL, 'Lorem Fusce facilisis vel sem et semper.
-                    Donec tempus tortor sed ultricies mollis.', CURRENT_TIMESTAMP);
+		(1, NULL, 'Juan Post 2', CURRENT_TIMESTAMP);
 
 INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
-		(20, NULL, 'In hac habitasse platea dictumst. Phasellus sit amet risus in ligula imperdiet
-                     tincidunt ut in diam.', CURRENT_TIMESTAMP);
+		(1, NULL, 'Juan Post 3', CURRENT_TIMESTAMP);
 
+INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
+		(22, NULL, 'Dan Post 1', CURRENT_TIMESTAMP);
+
+INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
+		(22, NULL, 'Dan Post 2', CURRENT_TIMESTAMP);
+
+INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
+		(22, NULL, 'Dan Post 3', CURRENT_TIMESTAMP);
+
+INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
+		(29, NULL, 'Jocelyn Post 1', CURRENT_TIMESTAMP);
+
+INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
+		(29, NULL, 'Jocelyn Post 2', CURRENT_TIMESTAMP);
+
+INSERT INTO Posts(AccountID, photoPath, textPost, postTime) VALUES
+		(29, NULL, 'Jocelyn Post 3', CURRENT_TIMESTAMP);
 
 -- Query to get all of a user's posts
 SELECT UserAccount.FirstName, UserAccount.LastName, UserAccount.AccountID, textPost
